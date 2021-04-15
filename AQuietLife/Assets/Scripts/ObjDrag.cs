0cf6d8bba45928dc647c7c49ebe393e9ad669d47
Manuel Geraldes <manuelgeraldes2009@hotmail.com>
2021-04-15 22:25:59 +0100

    Object drag script

---
+++ AQuietLife/Assets/Scripts/ObjDrag.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjDrag : MonoBehaviour
+{
+    private Vector3 mousePosition;
+    private Rigidbody2D rb;
+    private Vector2 direction;
+    private float moveSpeed = 100f;
+
+    // Use this for initialization
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 mousePos =
+                Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Vector2 mousePos2D = new Vector2(mousePos.x, mousePos.y);
+
+        RaycastHit2D hit = Physics2D.Raycast(mousePos2D, Vector2.zero);
+
+        if (Input.GetMouseButton(0) && hit.collider.CompareTag("Table"))
+        {
+            mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            direction = (mousePosition - transform.position).normalized;
+            rb.velocity = new Vector2(direction.x * moveSpeed, direction.y * moveSpeed);
+        }
+        else
+        {
+            rb.velocity = Vector2.zero;
+        }
+    }
+}
