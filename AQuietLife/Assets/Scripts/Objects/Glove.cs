f091d75a4788f50056e7043cae619011ea46e457
Manuel Geraldes <manuelgeraldes2009@hotmail.com>
2021-04-25 01:33:57 +0100

    Inventory system

---
+++ AQuietLife/Assets/Scripts/Objects/Glove.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class Glove : MonoBehaviour, IPointerClickHandler
+{
+    public bool hasGlove;
+    public bool gloveSelected;
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        gloveSelected = true;
+        FindObjectOfType<Plate>().plateSelected = false;
+    }
+
+    private void Awake()
+    {
+        hasGlove = true;
+    }
+}
---
+++ AQuietLife/Assets/Scripts/Objects/Plate.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class Plate : MonoBehaviour, IPointerClickHandler
+{
+    public bool hasPlate;
+    public bool plateSelected;
+    public bool plateUsed;
+
+    private void Update()
+    {
+        if (plateUsed == true)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        plateSelected = true;
+        FindObjectOfType<Glove>().gloveSelected = false;
+    }
+
+    private void Awake()
+    {
+        hasPlate = true;
+    }
+}
