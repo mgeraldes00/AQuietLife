d11decb0763f6ae1281b05d70c479fcb6a9cf5a6
Manuel Geraldes <manuelgeraldes2009@hotmail.com>
2021-08-18 14:51:48 +0100

    Fixed storage door interaction

--- AQuietLife/Assets/Scripts/CloseUp/Kitchen/CloseUpStorage.cs
+++ AQuietLife/Assets/Scripts/CloseUp/Kitchen/CloseUpStorage.cs
@@ -12,7 +12,7 @@ public class CloseUpStorage : MonoBehaviour
     public PolygonCollider2D lockedDoor;
     public BoxCollider2D keyHole;
 
-    private void OnMouseDown()
+    private void OnMouseUp()
     {
         if (gameMng.isLocked == false && zoom.currentView == 0)
         {
@@ -23,7 +23,7 @@ public class CloseUpStorage : MonoBehaviour
             if (storage.rightDoorCol == true)
                 StartCoroutine(storage.EnableObjs(1));
             StartCoroutine(TimeToZoom());
-        }          
+        }
     }
 
     private void OnMouseEnter()
@@ -41,23 +41,39 @@ public class CloseUpStorage : MonoBehaviour
     public void Normalize()
     {
         GetComponent<BoxCollider2D>().enabled = true;
-        /*for (int i = 0; i < doors.Length; i++)
-            doors[i].enabled = false;*/
+        for (int i = 0; i < doors.Length; i++)
+            doors[i].enabled = false;
+        if (storage.isLocked == true)
+            lockedDoor.enabled = false;
+        storage.isOnDoor = false;
     }
 
     IEnumerator TimeToZoom()
     {
         yield return new WaitForSeconds(0.1f);
         GetComponent<BoxCollider2D>().enabled = false;
+        storage.isOnDoor = true;
         if (storage.isLocked == true)
         {
             lockedDoor.enabled = true;
             doors[1].enabled = true;
             keyHole.enabled = true;
         }
-        else
-            for (int i = 0; i < doors.Length; i++)
-                doors[i].enabled = true;
+        else if (storage.isLocked == false)
+            if (storage.leftDoorCol == true && storage.rightDoorCol == false)
+                for (int i = 1; i < 3; i++)
+                    doors[i].enabled = true;
+            else if (storage.leftDoorCol == false && storage.rightDoorCol == true)
+            {
+                doors[0].enabled = true;
+                doors[3].enabled = true;
+            }
+            else if (storage.leftDoorCol == false && storage.rightDoorCol == false)
+                for (int i = 0; i < 2; i++)
+                    doors[i].enabled = true;
+            else if (storage.leftDoorCol == true && storage.rightDoorCol == true)
+                for (int i = 2; i < doors.Length; i++)
+                    doors[i].enabled = true;
         zoom.currentView++;
     }
 }
--- AQuietLife/Assets/Scripts/ObjManager/Kitchen/Storage/StorageManager.cs
+++ AQuietLife/Assets/Scripts/ObjManager/Kitchen/Storage/StorageManager.cs
@@ -13,16 +13,18 @@ public class StorageManager : MonoBehaviour
     private Vector3 mousePosition;
     [SerializeField] private Rigidbody2D[] rb;
     private Vector2 direction;
-    public float moveSpeed = 10f;
+    public float moveSpeed = 10.0f;
     public float moveSpeed2 = 10.0f;
 
     public GameObject[] objectsLeft;
     public GameObject[] objectsRight;
+    public GameObject[] doors;
 
     public bool leftDoorCol;
     public bool rightDoorCol;
 
     public bool isLocked;
+    public bool isOnDoor;
 
     [SerializeField] private bool isOnLock;
 
@@ -42,7 +44,7 @@ public class StorageManager : MonoBehaviour
 
             RaycastHit2D hit = Physics2D.Raycast(mousePos2D, Vector2.zero);
 
-            if (Input.GetMouseButton(0) && FindObjectOfType<GameManager>().isLocked == false
+            if (Input.GetMouseButtonUp(0) && FindObjectOfType<GameManager>().isLocked == false
                 )//Fix currentView
             {
                 if (hit.collider == null)
@@ -73,33 +75,87 @@ public class StorageManager : MonoBehaviour
                 {
                     if (isLocked == false)
                     {
-                        mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-                        direction = (mousePosition - transform.position).normalized;
-                        rb[0].velocity = new Vector2(direction.x * moveSpeed, direction.y * moveSpeed);
+                        StartCoroutine(QuickUnlock());
+                        //mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+                        //direction = (mousePosition - transform.position).normalized;
+                        //rb[0].velocity = new Vector2(direction.x * moveSpeed, direction.y * moveSpeed);
                         if (moveSpeed >= 0)
+                        {
+                            doors[0].GetComponent<BoxCollider2D>().enabled = false;
+                            doors[2].GetComponent<BoxCollider2D>().enabled = true;
+                            StartCoroutine
+                                (ObjectFade.FadeOut(doors[0].GetComponent<SpriteRenderer>(), 0));
+                            StartCoroutine
+                                (ObjectFade.FadeOut(doors[1].GetComponent<SpriteRenderer>(), 0));
+                            StartCoroutine
+                                (ObjectFade.FadeIn(doors[2].GetComponent<SpriteRenderer>()));
+                            StartCoroutine
+                                (ObjectFade.FadeIn(doors[3].GetComponent<SpriteRenderer>()));
                             FindObjectOfType<AudioCtrl>().Play("OpenStorage");
+                            moveSpeed = -10;
+                            leftDoorCol = true;
+                            StartCoroutine(EnableObjs(0));
+                        }
                         else if (moveSpeed <= 0)
+                        {
+                            doors[0].GetComponent<BoxCollider2D>().enabled = true;
+                            doors[2].GetComponent<BoxCollider2D>().enabled = false;
+                            StartCoroutine
+                                (ObjectFade.FadeIn(doors[0].GetComponent<SpriteRenderer>()));
+                            StartCoroutine
+                                (ObjectFade.FadeIn(doors[1].GetComponent<SpriteRenderer>()));
+                            StartCoroutine
+                                (ObjectFade.FadeOut(doors[2].GetComponent<SpriteRenderer>(), 0));
+                            StartCoroutine
+                                (ObjectFade.FadeOut(doors[3].GetComponent<SpriteRenderer>(), 0));
                             FindObjectOfType<AudioCtrl>().Play("CloseStorage");
+                            moveSpeed = 10;
+                            leftDoorCol = false;
+                            StartCoroutine(DisableObjs(0));
+                        }
                     }
                 }
                 else if (hit.collider.CompareTag("Direction2") && zoom.currentView == 1)
                 {
                     if (isLocked == false)
                     {
-                        mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-                        direction = (mousePosition - transform.position).normalized;
-                        rb[1].velocity = new Vector2(direction.x * moveSpeed2, direction.y * moveSpeed2);
+                        StartCoroutine(QuickUnlock());
+                        //mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+                        //direction = (mousePosition - transform.position).normalized;
+                        //rb[1].velocity = new Vector2(direction.x * moveSpeed2, direction.y * moveSpeed2);
                         if (moveSpeed2 >= 0)
+                        {
+                            doors[4].GetComponent<BoxCollider2D>().enabled = false;
+                            doors[5].GetComponent<BoxCollider2D>().enabled = true;
+                            StartCoroutine
+                                (ObjectFade.FadeOut(doors[4].GetComponent<SpriteRenderer>(), 0));
+                            StartCoroutine
+                                (ObjectFade.FadeIn(doors[5].GetComponent<SpriteRenderer>()));
                             FindObjectOfType<AudioCtrl>().Play("OpenStorage");
+                            moveSpeed2 = -10;
+                            rightDoorCol = true;
+                            StartCoroutine(EnableObjs(1));
+                        }
                         else if (moveSpeed2 <= 0)
+                        {
+                            doors[4].GetComponent<BoxCollider2D>().enabled = true;
+                            doors[5].GetComponent<BoxCollider2D>().enabled = false;
+                            StartCoroutine
+                                (ObjectFade.FadeIn(doors[4].GetComponent<SpriteRenderer>()));
+                            StartCoroutine
+                                (ObjectFade.FadeOut(doors[5].GetComponent<SpriteRenderer>(), 0));
                             FindObjectOfType<AudioCtrl>().Play("CloseStorage");
+                            moveSpeed2 = 10;
+                            rightDoorCol = false;
+                            StartCoroutine(DisableObjs(1));
+                        }
                     }
                 }
-                else
+                /*else
                 {
                     for (int i = 0; i < rb.Length; i++)
                         rb[i].velocity = Vector2.zero;
-                }
+                }*/
             }
         }
     }
@@ -108,7 +164,7 @@ public class StorageManager : MonoBehaviour
     {
         if (zoom.currentView == 1)
         {
-            if (rb[0].velocity == Vector2.zero)
+            if (isOnDoor == true)
             {
                 FindObjectOfType<CloseUpStorage>().Normalize();
                 StartCoroutine(DisableObjs(2));
@@ -165,5 +221,12 @@ public class StorageManager : MonoBehaviour
         closeUp.lockedDoor.enabled = false;
         closeUp.doors[0].enabled = true;
         closeUp.keyHole.enabled = false;
+    }
+
+    IEnumerator QuickUnlock()
+    {
+        isLocked = true;
+        yield return new WaitForSeconds(1.0f);
+        isLocked = false;
     }
 }
