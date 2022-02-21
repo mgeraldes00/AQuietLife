49a64e00a1ef1381f4f027771223efd5d62c026d
Manuel Geraldes <manuelgeraldes2009@hotmail.com>
2022-02-21 19:51:18 +0000

    Update Inspection script to allow for different cursor types

--- AQuietLife/Assets/Scripts/Temporary/Inspection.cs
+++ AQuietLife/Assets/Scripts/Temporary/Inspection.cs
@@ -4,13 +4,31 @@ using UnityEngine;
 
 public class Inspection : MonoBehaviour
 {
-    public ThoughtManager thought;
+    [SerializeField] private ThoughtManager thought;
+    [SerializeField] private GameManager gameMng;
 
     [SerializeField] private string inspectionText;
+    [SerializeField] private string cursorType;
 
     private void OnMouseDown()
     {
         thought.ShowThought();
         thought.text = inspectionText;
+    }
+
+    private void OnMouseEnter()
+    {
+        if (cursorType != null)
+        {
+            gameMng.cursors.ChangeCursor(cursorType, 1);
+        }
+    }
+
+    private void OnMouseExit()
+    {
+        if (cursorType != null)
+        {
+            gameMng.cursors.ChangeCursor(cursorType, 0);
+        }
     }
 }
